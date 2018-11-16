2b2d11b79b53638c7eb0c9717e253038e359e5e2
mezzelo <35051000+Mezzelo@users.noreply.github.com>
2018-11-15 22:15:45 -0500

    sample project upload
    uhhh scripts should be the same, except i just added the dice.  it's pretty rudimentary, you could clean that up to be on click or whatever.

---
+++ Assets/Scripts/Randomizer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomizer : MonoBehaviour {
+
+    public Sprite[] faces;
+
+    void OnMouseOver() {
+        Debug.Log((Time.time * 10f) % 1f);
+        if ((Time.time * 10f) % 1f < 0.2f)
+            gameObject.GetComponent<SpriteRenderer>().sprite = faces[Random.Range(0, faces.Length)];
+    }
+}
